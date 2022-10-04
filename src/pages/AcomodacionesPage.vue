c433d1d257736c076bf6be9ba9648391681bd089
yulieth-g <yuliethpaola345@gmail.com>
2022-10-03 20:51:45 -0500

    cambio

--- src/pages/AcomodacionesPage.vue
+++ src/pages/AcomodacionesPage.vue
@@ -29,7 +29,30 @@
                         <td>CUADRUPLE</td>
                     </tr>
                 </tbody>
+                <tr v-for="hotel in acomodaciones " :key="acomodaciones.id" class="">
+                    <td> {{acomodaciones.id}} </td>
+                    <td scope="row"> {{acomodaciones.nit}} </td>
+                    <td> {{acomodaciones.name}} </td>
+                    <td><button type="button" @click="viewacomodaciones(acomodaciones.id)"
+                            class="btn btn-warning rounded-5 text-light me-3">Detalle</button></td>
+                </tr>
+
+
+
             </table>
         </div>
     </article>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+    beforeMount() {
+        axios.get('http://ec2-44-201-108-206.compute-1.amazonaws.com/decameron/api/acomodaciones-types').then(response => (this.hoteles = response.data.data))
+    },
+    data() {
+        return {
+            acomodaciones: []
+        }
+    }
+}
